dd68ce93176e4566c067035b756264f5b0d3a905
Richard Guarnieri <richard.gm@outlook.com>
2022-06-11 12:48:37 -0400

    fixed typo on product model

--- models/Product.js
+++ models/Product.js
@@ -39,8 +39,8 @@ Product.init(
     categoryId: {
       type: DataTypes.INTEGER,
       references: {
-        model: category,
-        key: id 
+        model: 'category',
+        key: 'id' 
       }
     }
   },
